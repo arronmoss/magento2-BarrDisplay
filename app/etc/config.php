d8fe594ae52c94a04c675a9bc29b2f87e6513133
reubenszero <reuben.southall@zero1.co.uk>
2021-06-28 13:19:19 +0100

    https://zero1.teamwork.com/#/tasks/24380538

--- app/etc/config.php
+++ app/etc/config.php
@@ -359,6 +359,16 @@ return [
             'catalog' => [
                 'navigation' => [
                     'max_depth' => '2'
+                ],
+                'search' => [
+                    'engine' => 'elasticsuite'
+                ]
+            ],
+            'carriers' => [
+                'shipper' => [
+                    'api_key' => '0:3:TswdORKB0Nz/xlTNmnk028ymkvDtzH0RnO7Ik6b5Osv5O8iybverlXsoYZldPUEunZYM0ugaWmSmQjhDs1LuoocHyEGeeY3l7wL7tend5+LzBF4mV1uLcsXUG3jOMCOnZYe9lJkS8MAB7GDvctuIAQ==',
+                    'password' => '0:3:tfqXrS/4A61LHiw8644oua8e6buz4F91hNhvfxC7MlMBLe6kpcjUFJWz1LwyHIbQg1q6qOtbhjYl2GkufVi09gradAmHI7K1iwtzyMEsbpEoZCmKYYb1OyVlp+lZ5xEwNB1aGBgGCcqhLoD9f+lOsn4po3CGFW7K/KzaTfPuq0JOsUyt2stXHA==',
+                    'environment_scope' => 'TEST'
                 ]
             ]
         ]
